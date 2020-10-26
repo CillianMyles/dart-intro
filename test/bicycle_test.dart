92bc41ba87cfa7e511e1d20515f61ff7ec9700d5
Cillian Myles <myles.cillian@gmail.com>
2020-10-26 18:30:36 +0000

    bike tests | check initial state of other class variables;

--- test/bicycle_test.dart
+++ test/bicycle_test.dart
@@ -4,7 +4,9 @@ import 'package:test/test.dart';
 void main() {
   test('bike creation & speed evaluation', () {
     final bike = Bicycle(2, 1);
+    expect(bike.cadence, 2);
     expect(bike.speed, 0);
+    expect(bike.gear, 1);
 
     bike.speedUp(10);
     expect(bike.speed, 10);
