7a77cf1fe158f37bc21018a9aca24e4c18dd0e2d
Cillian Myles <myles.cillian@gmail.com>
2020-10-26 18:38:27 +0000

    improve test coverage for rectangle class;

--- test/rectangle_test.dart
+++ test/rectangle_test.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:dart_intro/rectangle.dart';
 import 'package:test/test.dart';
 
@@ -5,5 +7,8 @@ void main() {
   test('rectangle creation', () {
     final rectangle = Rectangle();
     expect(rectangle, isNotNull);
+    expect(rectangle.origin, Point(0, 0));
+    expect(rectangle.width, 0);
+    expect(rectangle.height, 0);
   });
 }
