dff66c4b4b4c922dd2a9cc033f391f6f9ef79a6c
Cillian Myles <myles.cillian@gmail.com>
2020-11-01 16:51:21 +0000

    move factory to base class; sub-classes now implement instead of extend;

--- bin/shape.dart
+++ bin/shape.dart
@@ -1,8 +1,8 @@
 import 'package:dart_intro/shape.dart';
 
 void main() {
-  final circle = shapeFactory('circle');
-  final square = shapeFactory('square');
+  final circle = Shape('circle');
+  final square = Shape('square');
   print(circle.area);
   print(square.area);
 }
--- lib/shape.dart
+++ lib/shape.dart
@@ -6,9 +6,15 @@ import 'package:meta/meta.dart';
 @immutable
 abstract class Shape {
   num get area;
+
+  factory Shape(String type) {
+    if (type == 'circle') return Circle(2);
+    if (type == 'square') return Square(2);
+    throw 'Cannot create shape of type: $type!';
+  }
 }
 
-class Square extends Shape {
+class Square implements Shape {
   final num side;
 
   Square(this.side);
@@ -17,18 +23,11 @@ class Square extends Shape {
   num get area => pow(side, 2);
 }
 
-class Circle extends Shape {
+class Circle implements Shape {
   final num radius;
 
   Circle(this.radius);
 
   @override
   num get area => pi * pow(radius, 2);
-}
-
-Shape shapeFactory(String type) {
-  ArgumentError.checkNotNull(type, 'type');
-  if (type == 'circle') return Circle(2);
-  if (type == 'square') return Square(2);
-  throw 'Cannot create shape of type: $type!';
 }
