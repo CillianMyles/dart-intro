14193d0b11e0fe8807a529cb4801adb307a37ff9
Cillian Myles <myles.cillian@gmail.com>
2020-11-01 17:19:23 +0000

    improve idiomatic-ness;

--- bin/04_functional.dart
+++ bin/04_functional.dart
@@ -2,5 +2,5 @@ import 'package:dart_intro/04_functional.dart';
 
 void main() {
   final values = [1, 2, 3, 5, 10, 50];
-  values.map((v) => scream(v)).forEach((i) => print(i));
+  values.map(scream).forEach(print);
 }
